3a1884f9238d508d5e9993d008dcd9763c9cd7f2
leandrobeandrade <leandrobe.andrade@gmail.com>
2024-11-29 23:42:41 -0300

    supabase init script

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
-let mainTitle = document.querySelector('#main-title');
-let mainNew = document.querySelectorAll('.new')[0];
-let sectionCardTitle = document.querySelectorAll('.card-title');
-let sectionCardNew = document.querySelectorAll('.card-text');
+const mainTitle = document.querySelector('#main-title');
+const mainNew = document.querySelectorAll('.new')[0];
+const sectionCardTitle = document.querySelectorAll('.card-title');
+const sectionCardNew = document.querySelectorAll('.card-text');
 
 function truncateWords(text, length) {
   const txt = text?.textContent?.slice(0, length);
@@ -23,3 +23,10 @@ function forToTruncate(elem_html, length) {
 
 forToTruncate(sectionCardTitle, 61);
 forToTruncate(sectionCardNew, 150);
+
+
+// const { createClient } = supabase
+// const supabaseUrl = 'https://hiqvrhtdkpjkqxkrasna.supabase.co'
+// const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhpcXZyaHRka3Bqa3F4a3Jhc25hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjA5MTI1ODEsImV4cCI6MjAzNjQ4ODU4MX0.X2jU3i1X3ua_VAU64vjQdP6erxl3B_cf622Xrx4uVJI'
+// const _supabase = C_C(supabaseUrl, supabaseKey)
+// console.log('Supabase Instance: ', _supabase)
