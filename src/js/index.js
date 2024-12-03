12d40917c3123e8311cd665a9d275a7ad715e0df
leandrobeandrade <leandrobe.andrade@gmail.com>
2024-12-03 00:31:24 -0300

    Ajustes nos arquivos js (indeex, truncate)

--- src/js/index.js
+++ src/js/index.js
@@ -1,30 +1,4 @@
-const mainTitle = document.querySelector('#main-title');
-const mainNew = document.querySelectorAll('.new')[0];
-const sectionCardTitle = document.querySelectorAll('.card-title');
-const sectionCardNew = document.querySelectorAll('.card-text');
-
-function truncateWords(text, length) {
-  const txt = text?.textContent?.slice(0, length);
-
-  if (text !== null) text.textContent = `${txt}...`;
-}
-
-truncateWords(mainTitle, 125);
-truncateWords(mainNew, 300);
-
-// Loop pelos elementos onde o truncate são feitos em vários elementos html
-function forToTruncate(elem_html, length) {
-  for (let i = 0; i < elem_html.length; i++) {
-    const elem = elem_html[i];
-    
-    truncateWords(elem, length);
-  }
-}
-
-forToTruncate(sectionCardTitle, 61);
-forToTruncate(sectionCardNew, 150);
-
-const journalist = document.querySelector('#journalist');
+let journalist = document.querySelector('#journalist');
 
 // Define o agente Supabase
 async function Supabase() {
@@ -32,7 +6,7 @@ async function Supabase() {
   .from('journalists')
   .select('name')
 
-  journalist.innerHTML = users?.data[0];
+  journalist.textContent = `Por: ${users?.data[0].name} em 26/07/2024 - 14:00`;
 }
 
 setTimeout(() => Supabase(), 500);
---
+++ src/js/utils/truncate.js
@@ -0,0 +1,26 @@
+const mainTitle = document.querySelector('#main-title');
+const mainNew = document.querySelectorAll('.new')[0];
+const sectionCardTitle = document.querySelectorAll('.card-title');
+// const sectionCardNew = document.querySelectorAll('.card-text');
+
+// Função de truncar os textos conforme o tamanho
+function truncateWords(text, length) {
+  const txt = text?.textContent?.slice(0, length);
+
+  if (text !== null) text.textContent = `${txt}...`;
+}
+
+truncateWords(mainTitle, 125);
+truncateWords(mainNew, 300);
+
+// Loop pelos elementos onde o truncate são feitos em vários elementos html
+function forToTruncate(elem_html, length) {
+  for (let i = 0; i < elem_html.length; i++) {
+    const elem = elem_html[i];
+    
+    truncateWords(elem, length);
+  }
+}
+
+forToTruncate(sectionCardTitle, 61);
+// forToTruncate(sectionCardNew, 180);
