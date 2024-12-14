85c7223035b5bc31df44023d003b00f8ea9344b5
leandrobeandrade <leandrobe.andrade@gmail.com>
2024-12-13 23:59:44 -0300

    Primeiras refatorações

--- src/js/news.js
+++ src/js/news.js
@@ -1,9 +1,28 @@
-let textTruncate = document.querySelector('#main-card');
+let journalist = document.querySelector('#journalist');
 
-function truncate() {
-  const txt = textTruncate?.textContent?.slice(0, 400);
+// Define o agente Supabase
+async function Supabase() {
+  const agent = window?._supabase;
 
-  if (textTruncate !== null) textTruncate.textContent = `${txt} ...`;
+  getMainNew(agent)
+
+  let users = await agent
+  .from('journalists')
+  .select('name')
+
+  journalist.textContent = `Por: ${users?.data[0].name} em 26/07/2024 - 14:00`;
 }
 
-truncate();
+async function getMainNew(agent) {
+  const new$ = await agent.from('news').select('*');
+  const title = document.querySelector('#main-title');
+  const img = document.querySelector('#img-main');
+  const new_ = document.querySelector('#main-new');
+
+  console.log(new$);
+  title.textContent = new$?.data[0].title;
+  img.setAttribute('src', new$?.data[0].image);
+  new_.textContent = new$?.data[0].new;
+}
+
+setTimeout(() => Supabase(), 500);
