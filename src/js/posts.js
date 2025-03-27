b26ad0b45f5c1b4a17a8855975e1b2c145811d03
leandrobeandrade <leandrobe.andrade@gmail.com>
2025-03-27 14:00:48 -0300

    Finalização das validações do formulário em postagens

--- src/js/posts.js
+++ src/js/posts.js
@@ -18,22 +18,34 @@ async function postNews() {
   const field1_ = document.getElementById('field1').value;
   const field2_ = document.getElementById('field2').value;
   const field3_ = document.getElementById('field3').value;
+  const invalid = document.getElementsByClassName('invalid');
 
-  const { data, error } = await agent_
-    .from('news')
-    .insert([
-      { title: title_, field1: field1_, field2: field2_, field3: field3_, id_journalist: 1 },
-    ])
-    .select()
+  for (let i = 0; i < invalid.length; i++) {
+    const element = invalid[i];
+    element.style.display = 'none';
+  }
 
-  if (error) console.log(error);
-  else console.log(data);
+  if (!title_) invalid[0].style.display = 'inline-block';
+  if (!field1_) invalid[1].style.display = 'inline-block';
+  if (!field2_) invalid[2].style.display = 'inline-block';
+  if (!field3_) invalid[3].style.display = 'inline-block';
+  else {
+    const { data, error } = await agent_
+      .from('news')
+      .insert([
+        { title: title_, field1: field1_, field2: field2_, field3: field3_, id_journalist: 1 },
+      ])
+      .select()
+  
+    if (error) console.log(error);
+    else console.log(data);
+  }
 }
 
 /**
  * Envia a notícia para o banco de dados ao clicar no botão
  */
-document.getElementById('post').addEventListener('click', () => console.log('postNews()'));
+document.getElementById('post').addEventListener('click', () => postNews());
 
 /**
  * Reseta os campos do formulário
